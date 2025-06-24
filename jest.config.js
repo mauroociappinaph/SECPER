b3ff8a2f71d1306142940ced874f9c8f24faaac8
Tu Nombre <usuario@ejemplo.com>
2025-06-24 14:13:04 -0300

    feat(tests): agregar configuración de Jest para pruebas unitarias en el módulo Google Drive

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const { createDefaultPreset } = require('ts-jest');
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+/** @type {import("jest").Config} **/
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/backend/google-drive/__tests__/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+  verbose: true,
+};
